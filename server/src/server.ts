6dc3fca73782b5272446154a8bb89d5264af24fd
Bruno Berval <brunoberval012@gmail.com>
2026-04-16 18:48:41 -0300

    fix: modulo costumer

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 // src/server.ts
+import 'dotenv/config'; // 👈 Adicione esta linha no topo de tudo!
 import app from './app';
 
 const PORT = process.env.PORT || 3000;
@@ -6,3 +7,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
+
